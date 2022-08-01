87b5000573dde8a0db06bc038ad4c0c09011271a
eduardosandrade <eduardo.mesquitacontabil@gmail.com>
2022-08-01 13:14:01 -0300

    metodos de lista

--- index.php
+++ index.php
@@ -2,15 +2,24 @@
 
 require_once("config.php");
 
-$joao = new Usuario();
+//carrega um usuario
+//$joao = new Usuario();
+//$joao->loadById(3);
+//echo $joao;
 
-$joao->loadById(3);
+//carrega uma lista de usuarios
+//$lista = Usuario::getList();
+//echo json_encode($lista);
 
-echo $joao;
+//carrega uma lista de usuarios buscando pelo login
+//$search = Usuario::search("rc");
+//echo json_encode($search);
 
+//carrega um usuario usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("bruno","12345");
 
-
-
+echo $usuario;
 
 
 
